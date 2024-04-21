95b09a2940166e5a6c17349454ef4b6ae2a6697e
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-04-21 13:09:53 +1000

    Added copy highlighting autocmd

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -39,4 +39,13 @@ M.neogen = require("custom.mappings.neogen")
 M.trouble = require("custom.mappings.trouble")
 M.venv_selector = require("custom.mappings.venv-selector")
 
+-- Autocmds
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 return M
